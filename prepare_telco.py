9efc2f317d055527f3c3a830213570197b92d14c
JaredVahle <jaredjohnvahle@gmail.com>
2021-08-26 18:12:17 -0500

    updating work

--- prepare_telco.py
+++ prepare_telco.py
@@ -1,13 +1,17 @@
 import pandas as pd
 
 def clean_telco(df):
+    
+    # replaces all the missing values with 0 and since all values are numeric, we change the total charges type to float
     df.total_charges.replace(to_replace = {" ":"0"}, inplace = True)
     df.total_charges = df.total_charges.astype("float")
+    # takes the payment type and checks for the word auto and creates a new column that represents a 1 or 0, 1 for if they have auto pay 0 for if they do not.
     df["auto_pay"] = df.payment_type.str.contains("auto")
     df["auto_pay"] = df.auto_pay.replace(to_replace = [True,False],value = [1,0])
+    # creating dummy variables for the catigorical values i can not easily change in pandas
     dummy_df = pd.get_dummies(df[['gender', 'payment_type',"internet_service_type","contract_type"]])
     dummy_df.columns = [col.lower().replace(" ","_") for col in dummy_df]
-    
+    # replacing the yes, no, and no service type to numeric values, (1 for yes) (0 for no, and 0 for no phone or internet service)
     df["partner"] = df.partner.replace(to_replace = ["Yes","No"],value = [1,0])
     df["dependents"] = df.dependents.replace(to_replace = ["Yes","No"],value = [1,0])
     df["churn"] = df.churn.replace(to_replace = ["Yes","No"],value = [1,0])
@@ -21,7 +25,9 @@ def clean_telco(df):
     df["streaming_movies"] = df.streaming_movies.replace(to_replace = ["Yes","No","No internet service"],value = [1,0,0])
     df["phone_service"] = df.phone_service.replace(to_replace = ["Yes","No","No internet service"],value = [1,0,0])
     df["multiple_lines"] = df.multiple_lines.replace(to_replace = ["Yes","No","No phone service"],value = [1,0,0])
+    # adding a column that sums the additional services and gives a total from (0-6)
     df["add_ons"] =  df.online_security + df.online_backup + df.device_protection + df.tech_support + df.streaming_tv + df.streaming_movies
+    # dropping columns that are duplicated by the dummy variables, the customer id, and id's that connected the different tables from sql
     df.drop(columns = ["customer_id",
                        "gender",
                        "payment_type",
@@ -35,6 +41,12 @@ def clean_telco(df):
                    "payment_type_id.1",
                    "Unnamed: 0"],inplace = True)
     return pd.concat([df, dummy_df], axis=1)
+
+def remove_unwanted_values(df):
+    # takes all the indexes where tenure is equal to 0
+    no_tenure = df[df.tenure == 0].index.to_list()
+    # drops all columns of the indexes given above
+    df.drop(index = no_tenure, inplace = True)
 
 def train_validate_test_split(df, target, seed=174):
     '''
@@ -51,5 +63,16 @@ def train_validate_test_split(df, target, seed=174):
     train, validate = train_test_split(train_validate, test_size=0.3, 
                                        random_state=seed,
                                        stratify=train_validate[target])
-    return train, validate, test
-    
+    return train, validate, test 
+
+def num_distributions(df):
+    '''
+    property of Faith Kane
+    This functions takes in a dataframe and displays
+    the distribution of each numeric column.
+    '''
+    for col in df.columns:
+        if df[col].dtype != 'object':
+            plt.hist(df[col])
+            plt.title(f'Distribution of {col}')
+            plt.show()
